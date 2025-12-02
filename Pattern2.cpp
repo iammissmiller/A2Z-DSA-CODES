0b1e9a1df96aa2a3da4730f5e044cb2b2f5c77b6
Praptee Miller <139339934+iammissmiller@users.noreply.github.com>
2025-12-02 23:13:49 +0530

    Pattern2.cpp

---
+++ Pattern2.cpp
@@ -0,0 +1,27 @@
+// 22 paterns using nested for loop
+// pattern 2
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+
+    int n ;
+
+    cin >> n;
+
+    for (int i = 0 ; i < n ; i++)
+    {
+        for(int j = 0 ; j < n ; j++)
+        {
+            cout << "* ";
+        }
+
+        cout << "\n";
+    }
+
+    return 0;
+
+}
