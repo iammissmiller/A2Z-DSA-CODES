2efa70e1cdda3b2759f06f8fae52f5349b4d6137
Praptee Miller <139339934+iammissmiller@users.noreply.github.com>
2025-12-12 23:16:29 +0530

    Pattern11.cpp

---
+++ Pattern11.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+
+    int n ; 
+    cin >> n;
+
+    for(int i = 1; i <= n; i++) {
+        int num = 1;
+        for(int j = 1; j <= i; j++) cout << num++ << " ";
+        cout << endl;
+    }
+    return 0;
+
+}
