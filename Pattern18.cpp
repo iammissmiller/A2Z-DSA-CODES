d8180f819d121cc6af59cd9c3c9ead4523dae8cf
Praptee Miller <139339934+iammissmiller@users.noreply.github.com>
2025-12-15 23:10:32 +0530

    Pattern18.cpp

---
+++ Pattern18.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+
+    int n ; 
+    cin >> n;
+
+    int space = 2*(n-1);
+
+    for(int i = 1; i <= 2*n - 1; i++) {
+
+        int stars = (i <= n) ? i : (2*n - i);
+
+        for(int j = 1; j <= stars; j++) cout << "*";
+
+        for(int j = 0; j < space; j++) cout << " ";
+
+        for(int j = 1; j <= stars; j++) cout << "*";
+
+        if(i < n) space -= 2;
+        else space += 2;
+
+        cout << endl;
+    }
+
+    return 0;
+
+}
