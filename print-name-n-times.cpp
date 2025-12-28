36ab7a446686493a7f72ddbed837336f46b97b8f
Praptee Miller <139339934+iammissmiller@users.noreply.github.com>
2025-12-28 16:41:54 +0530

    print-name-n-times.cpp

---
+++ print-name-n-times.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void name(int i, int n)
+{
+    if(i>n)
+    return;
+    else
+    cout << "Miller" << endl;
+    name(i+1,n);
+}
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    name(1,n);
+
+    return 0;
+}
