8cc185e27868ced3c8056ddf6d2a7b1d3f2a8dd3
Praptee Miller <139339934+iammissmiller@users.noreply.github.com>
2026-02-05 23:24:09 +0530

    check-array-is-sorted.cpp

---
+++ check-array-is-sorted.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isSorted(vector<int> a , int n)
+{
+    for(int i = 0; i < n ; i++)
+    {
+        if(a[i]<a[i+1] || a[i]== a[i+1])
+        {
+            
+        }
+        else return false;
+    }
+
+    return true;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for(int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    cout << isSorted(a,n);
+
+    return 0;
+}
+ 
