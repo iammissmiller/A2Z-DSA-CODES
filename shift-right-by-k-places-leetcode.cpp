fa26744f3a774fecd05a90aeed886c8219877c21
Praptee Miller <139339934+iammissmiller@users.noreply.github.com>
2026-02-06 23:45:46 +0530

    shift-right-by-k-places-leetcode.cpp

---
+++ shift-right-by-k-places-leetcode.cpp
@@ -0,0 +1,19 @@
+// using reverse function in <algorithm> library
+
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k % n;  // handles k > n
+
+        // reverse entire array
+        reverse(nums.begin(), nums.end());
+
+        // reverse first k elements
+        reverse(nums.begin(), nums.begin() + k);
+
+        // reverse remaining elements
+        reverse(nums.begin() + k, nums.end());
+    }
+};
