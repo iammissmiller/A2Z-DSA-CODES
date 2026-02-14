28de501a48ef0bf2a4afb57a85a6938a266928ac
Praptee Miller <139339934+iammissmiller@users.noreply.github.com>
2026-02-14 23:42:40 +0530

    Union-Brute.cpp

---
+++ Union-Brute.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> unionBrute(vector<int> &a , vector<int> &b) 
+{
+    
+    set<int> s;
+
+    for(int x : a)
+    s.insert(x);
+
+    for(int x : b)
+    s.insert(x);
+
+    vector<int> result(s.begin() , s.end());
+
+    return result;
+   
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    vector<int> b(n);
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> b[i];
+    }
+
+    vector<int> result = unionBrute(a,b);
+
+    for(int i = 0; i < result.size(); i++)
+    {
+        cout << result[i] << " ";
+    }
+
+
+    return 0;
+}
