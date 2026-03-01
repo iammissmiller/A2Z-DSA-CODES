636509a68d70801212c79439ce1f129e333644a3
Praptee Miller <139339934+iammissmiller@users.noreply.github.com>
2026-03-01 23:21:33 +0530

    longest-subarray-brute.cpp

---
+++ longest-subarray-brute.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int longestsubarraybrute(vector<int> &a, int k) 
+{
+    int n = a.size();
+    int maxlen = 0;
+
+    for(int i = 0; i < n ; i++)
+    {
+        int sum = 0;
+
+        for(int j = i ; j<n ; j++)
+        {
+            sum = sum + a[j];
+
+            if(sum==k)
+            {
+                maxlen = max(maxlen , j-i+1);
+            }
+
+            
+        }
+    }
+
+    return maxlen;
+   
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    int k;
+    cin >> k;
+
+    int result =  longestsubarraybrute(a,k);
+
+    cout << result;
+
+    return 0;
+}
