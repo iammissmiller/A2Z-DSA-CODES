de8831a462fcfd5d9bf00640741e5bc47f8f612f
Praptee Miller <139339934+iammissmiller@users.noreply.github.com>
2026-05-10 22:11:01 +0530

    count-occurrences-brute.cpp

---
+++ count-occurrences-brute.cpp
@@ -0,0 +1,12 @@
+int countOccurrences(vector<int>& arr, int x) {
+
+    int count = 0;
+
+    for(int i = 0; i < arr.size(); i++) {
+
+        if(arr[i] == x)
+            count++;
+    }
+
+    return count;
+}
