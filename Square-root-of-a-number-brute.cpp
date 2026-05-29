cffc763a59fd877db795c722d94625bf35260cc8
Praptee Miller <139339934+iammissmiller@users.noreply.github.com>
2026-05-29 23:59:08 +0530

    Implement brute force method to find square root

---
+++ Square-root-of-a-number-brute.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int ans = 0;
+
+    for (int i = 1; i * i <= n; i++) {
+        ans = i;
+    }
+
+    cout << ans << endl;   // Floor value of square root
+    return 0;
+}
