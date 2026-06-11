829d260fd1836ad713d15fbc776cf14e71d11070
Praptee Miller <139339934+iammissmiller@users.noreply.github.com>
2026-06-12 00:37:50 +0530

    find-pivot-index-leetcode.cpp

---
+++ Arrays/find-pivot-index-leetcode.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int totalsum = 0;
+
+        for(int num : nums)
+        {
+            totalsum = num + totalsum;
+        }
+
+        int leftsum = 0;
+
+        for(int i= 0 ; i < nums.size() ; i++)
+        {
+            int rightsum = totalsum - leftsum - nums[i];
+
+            if(leftsum == rightsum)
+            return i;
+
+            leftsum = leftsum + nums[i];
+        }
+
+        return -1;
+    }
+};
