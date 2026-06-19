468281289256815ef3c0f526509e33e92d53a69d
Praptee Miller <139339934+iammissmiller@users.noreply.github.com>
2026-06-20 00:12:56 +0530

    Implement subarray sum equals k solution

---
+++ Strings/subarray-sum-equals-k-leetcode.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int> freq;
+        freq[0] = 1;
+
+        int prefixsum = 0;
+        int count = 0;
+
+        for(int num : nums)
+        {
+            prefixsum += num;
+
+            count += freq[prefixsum - k];
+
+            freq[prefixsum]++;
+        }
+
+        return count;
+    }
+};
