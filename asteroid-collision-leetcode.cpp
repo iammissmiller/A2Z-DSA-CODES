ac4671b18f6cf2884dbd41d6a9c3d716c5f25687
Praptee Miller <139339934+iammissmiller@users.noreply.github.com>
2026-07-17 23:13:02 +0530

    Add asteroid collision solution using stack
    Implement asteroid collision logic using a stack to handle collisions between moving asteroids.

---
+++ asteroid-collision-leetcode.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int> st;
+
+        for (int asteroid : asteroids) {
+
+            while (!st.empty() &&
+                   st.top() > 0 &&
+                   asteroid < 0) {
+
+                if (st.top() < -asteroid) {
+                    st.pop();
+                }
+                else if (st.top() == -asteroid) {
+                    st.pop();
+                    asteroid = 0;
+                }
+                else {
+                    asteroid = 0;
+                }
+            }
+
+            if (asteroid != 0) {
+                st.push(asteroid);
+            }
+        }
+
+        vector<int> ans(st.size());
+
+        for (int i = st.size() - 1; i >= 0; i--) {
+            ans[i] = st.top();
+            st.pop();
+        }
+
+        return ans;
+    }
+};
